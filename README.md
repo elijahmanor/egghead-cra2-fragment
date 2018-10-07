aca318d7d0519c4cb5116c08ffbce7d8a762920f
Elijah Manor <elijah.manor@gmail.com>
2018-10-06 23:32:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# egghead-cra2-fragment
+This contains the code for the egghead.io lesson “Use the Fragment Short Syntax in Create React App 2.0”
---
+++ cra2-fragment/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component, Fragment } from "react";
+import "./App.css";
+
+const Content = () => (
+  <>
+    <nav>Navigation</nav>
+    <main>Main area</main>
+  </>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>Header</header>
+        <Content />
+        <footer>Footer</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
